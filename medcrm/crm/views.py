1f12f72d2775d5b3f5dbfb91bbecfbb3925208c8
francisdbillones <francisd.billones@gmail.com>
2022-07-13 21:03:36 +0800

    add requirements

--- medcrm/crm/views.py
+++ medcrm/crm/views.py
@@ -1,14 +1,12 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 from django.views import generic
-from django.http import HttpResponse
-from django.core.handlers.asgi import ASGIRequest
+from django.http import HttpRequest, HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView as DjangoLoginView
 
-# Create your views here.
-class IndexView(LoginRequiredMixin, generic.TemplateView):
-    template_name = "dashboard.html"
+def index(r: HttpRequest) -> HttpResponse:
+    return "hello"
 
-
-class LoginView(DjangoLoginView):
-    template_name = "login.html"
+def login(r: HttpResponse) -> HTTPResponse:
+    return "login"
