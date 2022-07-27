11d7371cab32f493f1cc9a1dd55bbaca01dcf26c
francisdbillones <francisd.billones@gmail.com>
2022-07-27 10:18:36 +0800

    move agent to actual agents app

--- agents/models.py
+++ agents/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from leads.models import User, UserProfile
+
+
+class Agent(AbstractUser):
+    # fields already defined by AbstractUser:
+    # username
+    # first_name
+    # last_name
+    # email
+    # is_staff
+    # is_active
+    # date_joined
+
+    phone = models.CharField(
+        max_length=15,
+        null=True,
+        blank=False,
+        validators=[lambda s: all(c.isdigit() for c in s)],
+    )
+    profile_picture = models.ImageField(upload_to="agent_pfps", null=True, blank=True)
+
+    def __str__(self):
+        return self.user.email
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(email__isnull=False) | models.Q(phone__isnull=False),
+                name="email_or_phone_not_null",
+            )
+        ]
--- leads/models.py
+++ leads/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
+from agents.models import Agent
+
 
 class User(AbstractUser):
     is_organisor = models.BooleanField(default=True)
@@ -26,12 +28,20 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
-    category = models.ForeignKey("Category", related_name="leads", null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        "Category",
+        related_name="leads",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
     description = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
     phone_number = models.CharField(max_length=20)
     email = models.EmailField()
-    profile_picture = models.ImageField(null=True, blank=True, upload_to="profile_pictures/")
+    profile_picture = models.ImageField(
+        null=True, blank=True, upload_to="profile_pictures/"
+    )
     converted_date = models.DateTimeField(null=True, blank=True)
 
     objects = LeadManager()
@@ -52,14 +62,6 @@ class FollowUp(models.Model):
 
     def __str__(self):
         return f"{self.lead.first_name} {self.lead.last_name}"
-
-
-class Agent(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user.email
 
 
 class Category(models.Model):
